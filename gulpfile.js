96a83d5926c8baf113a6b67f2e0448083bb62958
designerkarigar <designerkarigar@gmail.com>
2024-04-07 02:16:48 +0530

    gulp1

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,12 @@ requireDir('./gulp');
 |  Compile
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 gulp.task('compile', gulp.parallel('style', 'script', 'vendor'));
-gulp.task('compile', gulp.parallel('compile', 'pug'));
+gulp.task('compile:all', gulp.parallel('compile', 'pug'));
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 |  Deploy
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('build', gulp.series('clean:build', 'build:static', 'compile'));
+gulp.task('build', gulp.series('clean:build', 'build:static', 'compile:all'));
 gulp.task('build:test', gulp.series('build', 'watch'));
 gulp.task('live', gulp.series('clean:live', 'build', 'build:push'));
 
@@ -24,4 +24,4 @@ gulp.task('default', gulp.series('clean', 'compile', 'watch'));
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 |  Product
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('product:make', gulp.series('compile', 'product'));
+gulp.task('product:make', gulp.series('compile:all', 'product'));
